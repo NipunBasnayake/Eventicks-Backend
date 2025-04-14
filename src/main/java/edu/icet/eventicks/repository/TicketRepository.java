507018b22baf4e600f30ec28a605bd0d71215df9
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-04-14 22:42:25 +0530

    Ticket service

--- src/main/java/edu/icet/eventicks/repository/TicketRepository.java
+++ src/main/java/edu/icet/eventicks/repository/TicketRepository.java
@@ -1,8 +1,17 @@
 package edu.icet.eventicks.repository;
 
+import edu.icet.eventicks.dto.TicketDto;
 import edu.icet.eventicks.entity.TicketEntity;
+import edu.icet.eventicks.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface TicketRepository extends JpaRepository<TicketEntity, Integer> {
 
+    List<TicketEntity> findByEventId(Integer eventId);
+
+
+    List<TicketDto> findBySeller(Optional<UserEntity> seller);
 }
