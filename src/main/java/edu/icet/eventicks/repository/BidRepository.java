ad20463a0c2fcc81e70865688947adb4977d6789
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-04-17 00:22:08 +0530

    Implemented Dashboard Service

--- src/main/java/edu/icet/eventicks/repository/BidRepository.java
+++ src/main/java/edu/icet/eventicks/repository/BidRepository.java
@@ -7,9 +7,14 @@ import edu.icet.eventicks.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface BidRepository extends JpaRepository<BidEntity, Integer> {
     Collection<Object> findByUser(UserEntity user);
 
     Collection<Object> findByTicket(TicketEntity ticket);
+
+    Collection<? extends BidEntity> findByTicketTicketId(Integer ticketId);
+
+    List<BidEntity> findByUserUserId(Integer userId);
 }
--- src/main/java/edu/icet/eventicks/repository/EventRepository.java
+++ src/main/java/edu/icet/eventicks/repository/EventRepository.java
@@ -4,8 +4,14 @@ import edu.icet.eventicks.entity.EventEntity;
 import edu.icet.eventicks.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.List;
 
 public interface EventRepository extends JpaRepository<EventEntity, Integer> {
     Collection<Object> findByCreatedBy(UserEntity createdBy);
+
+    List<EventEntity> findByEventDateAfter(LocalDateTime now);
+
+    List<EventEntity> findByCreatedByUserId(Integer userId);
 }
